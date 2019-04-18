4436622d8a622058e0463cb6f4cb2b2938d404f2
Agentvm <Agentvm@users.noreply.github.com>
2019-04-18 16:40:00 +0200

    Add files via upload

---
+++ workflow.py
@@ -0,0 +1,11 @@
+import sys
+#import matplotlib
+#from sklearn import datasets
+#import cv2
+import pcl
+
+print ('executed with python version ' + str (sys.version_info[0] ) + '.' + str(sys.version_info[1]) + "\n" )
+
+
+#"laserscanning.ply"
+print ('Done.')
