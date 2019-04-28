b9c08c492822cd1bd6b75046b0b987873d248e50
agentvm <jannik.busse@onlinehome.de>
2019-04-28 15:14:45 +0200

    workflow: added numpy_to_pcl (), tested load_las_file (), experimented with matplotlib (see snippets) snippets: Added code dump test.py: script for quick testing

---
+++ test.py
@@ -0,0 +1,9 @@
+# script for qick testing
+
+import numpy as np
+
+array = np.array([[1, 2, 3, 4], [5, 6, 7, 8]], dtype=np.float32)
+
+print (array)
+print (array[:, 0:3])
+#print (array.reshape (-1, 3))
--- workflow.py
+++ workflow.py
@@ -1,18 +1,22 @@
+# for testing and demonstration of point cloud workflows
+
+# os
 import sys
 #import os
+
+# data transformation
 import numpy as np
-from laspy.file import File
-import matplotlib
 import pcl
 
+# load
+from laspy.file import File
 
-# useful stuff
-#   num_returns = inFile.num_returns    # number of returns
-#   return_num = inFile.return_num      # this points return number
-#   ground_points = inFile.points[num_returns == return_num]
-#
-#   inFile.intensity
-def load_las(dir_in, filename, dtype = None):
+# visualization
+#from matplotlib import pyplot
+#from mpl_toolkits.mplot3d import Axes3D
+
+
+def load_las_file (dir_in, filename, dtype=None):
     """
     Loads .las data as numpy array
 
@@ -30,12 +34,12 @@ def load_las(dir_in, filename, dtype = None):
     """
 
     # load tile
+    print('Loading file ' + file_name + ' ...')
     with File(dir_in + filename, mode = 'r') as inFile:
-        x = np.reshape(inFile.x.copy(), (-1, 1)) # create colums
+        x = np.reshape(inFile.x.copy(), (-1, 1))    # create colums
         y = np.reshape(inFile.y.copy(), (-1, 1))
         z = np.reshape(inFile.z.copy(), (-1, 1))
         raw_class = np.reshape(inFile.raw_classification.copy(), (-1, 1))
-
 
         if dtype == 'dim':
             red = np.reshape(inFile.red.copy(), (-1, 1))    # add rgb
@@ -50,79 +54,61 @@ def load_las(dir_in, filename, dtype = None):
         else:
             points = np.concatenate((x, y, z, raw_class), axis = -1)
 
+    print ('Cloud imported.\nNumber of points: ' + str(points.size ) + '\n')
     return points
 
 
+def numpy_to_pcl (input_cloud_numpy):
+    """
+    Takes a numpy array and returns a pcl cloud
+
+    Input:
+        input_cloud_numpy: numpy array with data points and Intensity or RGB values
+
+    Output:
+        pcl_cloud: A pcl cloud
+    """
+
+    # get number of different values XYZRGB, XYZI or XYZ
+    numpy_colums = input_cloud_numpy.shape[1]
+    if (numpy_colums < 3 ):
+        return pcl.PointCloudXYZ ()     # abort
+
+    if (numpy_colums == 6):     # DIM cloud, with RGB
+        # Python understands float as Float64, which C++ understands as Double, therefore, a conversion is needed.
+        pcl_cloud = pcl.PointCloudXYZRGB(np.array(input_cloud_numpy, dtype=np.float32 ))
+    if (numpy_colums == 4):     # ALS cloud, with intensity
+        # Python understands float as Float64, which C++ understands as Double, therefore, a conversion is needed.
+        pcl_cloud = pcl.PointCloud_PointXYZI (np.array(input_cloud_numpy, dtype=np.float32 ))
+    else:   # some other cloud
+        first_three_colums = input_cloud_numpy[:, 0:3]
+        pcl_cloud = pcl.PointCloudXYZ(np.array(first_three_colums, dtype=np.float32 ))
+
+    return pcl_cloud
 
 
+if __name__ == '__main__':
 
-print ('executed with python version ' + str (sys.version_info[0] ) + '.' + str(sys.version_info[1]) + "\n" )
+    print ('executed with python version ' + str (sys.version_info[0] ) + '.' + str(sys.version_info[1]) + "\n" )
 
-# Loading
-clouds_folder = 'clouds/ALS2014/'
-file_name = '33314059950_05_2014-01-03.las'
+    # Files
+    #clouds_folder = 'clouds/ALS2014/'
+    #file_name = '33314059950_05_2014-01-03.las'
 
-print('Loading file ' + file_name + ' ...')
-input_cloud = load_las (clouds_folder, file_name )
-#input_cloud = pcl.load(file_name)
-print ('Cloud imported.\nNumber of points: ' + str(input_cloud.size ))
+    clouds_folder = 'clouds/DIM2016/'
+    file_name = 'DSM_Cloud_333140_59950.las'
 
-print ('\n Make pcl cloud')
-cloud = pcl.PointCloud_PointXYZRGB()
+    #clouds_folder = 'clouds/'
+    #file_name = 'laserscanning.ply'
 
+    # Loading
+    numpy_cloud = load_las_file (clouds_folder, file_name )
 
+    print (str(numpy_cloud ) + "\n\n" )
 
+    pcl_cloud = numpy_to_pcl (numpy_cloud )
+    #pcl_cloud = pcl.load(str(clouds_folder + file_name ))
 
+    print (str(pcl_cloud[:, :, :] ) + "\n\n" ) # how to print a pcl cloud??
 
-#########################################
-# try this
-np_cloud = np.empty([pcl_cloud.width, 4], dtype=np.float32)
-np_cloud = np.asarray(pcl_cloud)
-np_cloud = pcl_cloud.to_array()
-
-
-
-#########################################
-# or this
-p = pcl.PointCloud(np.array([[1, 2, 3], [3, 4, 5]], dtype=np.float32))
-seg = p.make_segmenter()
-seg.set_model_type(pcl.SACMODEL_PLANE)
-seg.set_method_type(pcl.SAC_RANSAC)
-indices, model = seg.segment()
-
-# print (input_cloud.shape)
-# print (type (input_cloud[0,0]) )
-# for value in input_cloud:
-#    value = np.float32 (value )
-#
-# #input_cloud.astype (float)
-# print (type (input_cloud[0,0]) )
-#
-# cloud.from_array(input_cloud )
-#
-# print ('\nDone.')
-
-
-
-
-# def load_cloud (path_to_file, file_name ):
-#     """
-#     Wrapping the pcl load and custom .lsa load function
-#     """
-#
-#     print('Loading file ' + file_name + '.')
-#     input_cloud = None;
-#
-#     # check for file extension to determine the function used for loading
-#     file_extension = os.path.splitext (file_name )[1]
-#     if (file_extension == '.las'):
-#         input_cloud = load_las ()
-#     else:
-#         input_cloud = pcl.load(file_name)
-#
-#     if (input_cloud != None ):
-#         print('Cloud imported.\nNumber of points: ' + str(input_cloud.size ))
-#     else:
-#         print ('Cloud import error');
-#
-#     return input_cloud
+    #pcl.save (pcl_cloud, str(clouds_folder) + 'output.ply')
