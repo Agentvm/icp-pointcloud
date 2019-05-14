19fcc865885a715a318d3c7e13f0089b4c33cee3
luh_busse <jannik.busse@onlinehome.de>
2019-05-14 15:49:21 +0200

    Changed conversion to pcl cloud
    ... by reducing the cloud by it's mininmal x and y coordinates, which 
    are returned along with the pcl cloud.

--- conversions.py
+++ conversions.py
@@ -4,13 +4,16 @@ import numpy as np
 
 def numpy_to_pcl (input_cloud_numpy):
     """
-    Takes a numpy array and returns a pcl cloud
+    Takes a numpy array and returns a pcl cloud, and the values that have been subtracted from the cloud to fit the
+    values into a float32.
 
     Input:
         input_cloud_numpy (np.array): numpy array with data points and Intensity or RGB values
 
     Output:
-        pcl_cloud (pcl.PointCloudXYZ): A pcl cloud
+        pcl_cloud (pcl.PointCloudXYZ):  A pcl cloud
+        min_x (float):                  The amount by which the x coordinates have been reduced
+        min_y (float):                  The amount by which the y coordinates have been reduced
     """
 
     # get number of different values XYZRGB, XYZI or XYZ
@@ -19,6 +22,12 @@ def numpy_to_pcl (input_cloud_numpy):
         print ('In numpy_to_pcl: Inserted numpy cloud only has ' + str(numpy_colums)
                + ' channels. Returning empty cloud.')
         return pcl.PointCloudXYZ ()     # abort
+
+    # reduce coordinates to ensure that the precision of float32 is enough
+    min_x_coordinate = input_cloud_numpy[:, 0].min
+    min_y_coordinate = input_cloud_numpy[:, 1].min
+    input_cloud_numpy[:, 0] = input_cloud_numpy[:, 0] - min_x_coordinate
+    input_cloud_numpy[:, 1] = input_cloud_numpy[:, 1] - min_y_coordinate
 
     # DIM cloud, with RGB
     if (numpy_colums == 6):
@@ -34,7 +43,7 @@ def numpy_to_pcl (input_cloud_numpy):
         # Python understands float as Float64, which C++ understands as Double, therefore, a conversion is needed.
         pcl_cloud = pcl.PointCloudXYZ(np.array(first_three_colums, dtype=np.float32 ))
 
-    return pcl_cloud
+    return pcl_cloud, min_x_coordinate, min_y_coordinate
 
 
 def pcl_to_numpy (pcl_cloud ):
