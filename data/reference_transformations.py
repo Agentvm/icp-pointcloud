a46cb473093699e8de3a9a35628c6428c62a114c
Agentvm <pc.gamer@hotmail.de>
2019-05-23 17:06:11 +0200

    Added data/reference_transformations.py
    The CloudCompare Alignment Data from Point Picking is now saved in this 
    Dictionary for quick comparison after an icp run.

---
+++ data/reference_transformations.py
@@ -0,0 +1,104 @@
+'''
+Contains reference translations determined by point picking alignment for all extracted regions.
+Format: (translation_x, translation_y, translation_z, RMS)
+'''
+
+translations = {
+
+    # Color Houses
+    'clouds/Regions/Color Houses/ALS16_Cloud_reference_reduced_normals_cleared.asc':
+    ((0, 0, 0), 0),
+    'clouds/Regions/Color Houses/DSM_Cloud_reduced_normals.asc':
+    ((0, 0, 0), 0),
+
+    # DIM showcase
+    'clouds/Regions/DIM showcase/ALS16_Cloud_reference_reduced_normals_cleared.asc':
+    ((0, 0, 0), 0),
+    'clouds/Regions/DIM showcase/DSM_Cloud_reduced_normals.asc':
+    ((0, 0, 0), 0),
+
+    # Everything
+    'clouds/Regions/Everything/ALS14_Cloud_reference_reduced_normals_cleared.asc':
+    ((0, 0, 0), 0),
+    'clouds/Regions/Everything/ALS16_Cloud _Scan54_reduced_normals_cleared.asc':
+    ((-0.091762542725, 0.100151062012, -0.009692192078), 0.135153),
+    'clouds/Regions/Everything/ALS16_Cloud _Scan55_reduced_normals_cleared.asc':
+    ((-0.043861389160, 0.001186370850, -0.032357215881), 0.0969821),
+    'clouds/Regions/Everything/DSM_Cloud_reduced_normals.asc':
+    ((0.015129089355, -0.034282684326, 0.209009647369), 0.0862935),
+
+    # Field
+    'clouds/Regions/Field/ALS16_Cloud_Scan53_reference_reduced_normals_cleared.asc':
+    ((0, 0, 0), 0),
+    'clouds/Regions/Field/ALS16_Cloud_Scan54_reduced_normals_cleared.asc':
+    ((0, 0, 0), 0),
+    'clouds/Regions/Field/DSM_Cloud_reduced_normals.asc':
+    ((0, 0, 0), 0),
+
+    # Forest
+    'clouds/Regions/Forest/ALS16_Cloud_Scan46_reference_reduced_normals_cleared.asc':
+    ((0, 0, 0), 0),
+    'clouds/Regions/Forest/ALS16_Cloud_Scan47_reduced_normals_cleared.asc':
+    ((0, 0, 0), 0),
+    'clouds/Regions/Forest/DSM_Cloud_reduced_normals.asc':
+    ((0, 0, 0), 0),
+
+    # Missing Building
+    'clouds/Regions/Missing Building/ALS14_Cloud_reference_reduced_normals_cleared.asc':
+    ((0, 0, 0), 0),
+    'clouds/Regions/Missing Building/ALS16_Cloud_Scan48_reduced_normals_cleared.asc':
+    ((0, 0, 0), 0),
+    'clouds/Regions/Missing Building/ALS16_Cloud_Scan49_reduced_normals_cleared.asc':
+    ((0, 0, 0), 0),
+    'clouds/Regions/Missing Building/DSM_Cloud_reduced_normals.asc':
+    ((0, 0, 0), 0),
+
+    # Road
+    'clouds/Regions/Road/ALS16_Cloud_Scan54_reference_reduced_normals_cleared.asc':
+    ((0, 0, 0), 0),
+    'clouds/Regions/Road/ALS16_Cloud_Scan55_reduced_normals_cleared.asc':
+    ((0, 0, 0), 0),
+    'clouds/Regions/Road/DSM_Cloud_reduced_normals.asc':
+    ((0, 0, 0), 0),
+
+    # Test Xy
+    'clouds/Regions/Test Xy/ALS14_Cloud_reduced_normals_cleared.asc':
+    ((0, 0, 0), 0),
+
+    # Xy Tower
+    'clouds/Regions/Xy Tower/ALS16_Cloud_Scan54_reference_reduced_normals_cleared.asc':
+    ((0, 0, 0), 0),
+    'clouds/Regions/Xy Tower/ALS16_Cloud_Scan55_reduced_normals_cleared.asc':
+    ((0, 0, 0), 0),
+    'clouds/Regions/Xy Tower/DSM_Cloud_reduced_normals.asc':
+    ((0, 0, 0), 0),
+
+    # Xyz Square
+    'clouds/Regions/Xyz Square/ALS16_Cloud_Scan54_reference_reduced_normals_cleared.asc':
+    ((0, 0, 0), 0),
+    'clouds/Regions/Xyz Square/ALS16_Cloud_Scan55_reduced_normals_cleared.asc':
+    ((0, 0, 0), 0),
+    'clouds/Regions/Xyz Square/DSM_Cloud_reduced_normals.asc':
+    ((0, 0, 0), 0),
+
+    # Xz Hall
+    'clouds/Regions/Xz Hall/ALS16_Cloud_Scan53_reference_reduced_normals_cleared.asc':
+    ((0, 0, 0), 0),
+    'clouds/Regions/Xz Hall/ALS16_Cloud_Scan54_reduced_normals_cleared.asc':
+    ((0, 0, 0), 0),
+    'clouds/Regions/Xz Hall/DSM_Cloud_reduced_normals.asc':
+    ((0, 0, 0), 0),
+
+    # Yz Houses
+    'clouds/Regions/Yz Houses/ALS16_Cloud_reference_reduced_normals_cleared.asc':
+    ((0, 0, 0), 0),
+    'clouds/Regions/Yz Houses/DSM_Cloud_reduced_normals.asc':
+    ((0, 0, 0), 0),
+
+    # Yz Street
+    'clouds/Regions/Yz Street/ALS16_Cloud_reference_reduced_normals_cleared.asc':
+    ((0, 0, 0), 0),
+    'clouds/Regions/Yz Street/DSM_Cloud_reduced_normals.asc':
+    ((0, 0, 0), 0)
+
+}
