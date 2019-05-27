281d855e710f7fe734aed5775c2c1d27833e58b7
luh_busse <jannik.busse@onlinehome.de>
2019-05-27 18:38:16 +0200

    Refactored process_all_clouds.py
    ... and added ideas for automatic icp computation.

--- process_all_clouds.py
+++ process_all_clouds.py
@@ -64,6 +64,8 @@ def compute_normals (numpy_cloud, file_path, field_labels_list, query_radius ):
     list_of_point_indices = tree.query_radius(numpy_cloud, r=query_radius )
     additional_values = np.zeros ((numpy_cloud.shape[0], 4 ))
 
+    success = True
+
     # compute normals for each point
     for index, point_neighbor_indices in enumerate (list_of_point_indices ):
 
@@ -71,9 +73,10 @@ def compute_normals (numpy_cloud, file_path, field_labels_list, query_radius ):
         if (psutil.virtual_memory().percent > 95.0):
             print (print ("!!! Memory Usage too high: "
                           + str(psutil.virtual_memory().percent)
-                          + "%. Breaking loop. There still are "
+                          + "%. Skipping cloud. There still are "
                           + str (len (list_of_point_indices) - index)
-                          + " normal vectors left to compute."))
+                          + " normal vectors left to compute. Reduction process might be lost."))
+            success = False
             break
 
         # you can't estimate a cloud with less than three neighbors
@@ -92,7 +95,7 @@ def compute_normals (numpy_cloud, file_path, field_labels_list, query_radius ):
         additional_values[index, :] = np.append (normal_vector, sigma )
 
     # delete normals if already computed # refactor
-    if ('Nx' in field_labels_list ):
+    if ('Nx' in field_labels_list and success ):
         field_labels_list = field_labels_list[:-4]
         numpy_cloud = numpy_cloud[:, :-4]
 
@@ -100,7 +103,7 @@ def compute_normals (numpy_cloud, file_path, field_labels_list, query_radius ):
     numpy_cloud = np.concatenate ((numpy_cloud, additional_values ), axis=1 )
     field_labels_list.append('Nx ' 'Ny ' 'Nz ' 'Sigma' )
 
-    return numpy_cloud, field_labels_list
+    return numpy_cloud, field_labels_list, success
 
 
 def do_icp (numpy_reference_cloud, numpy_aligned_cloud, full_path ):
@@ -144,6 +147,81 @@ def compare_icp_results (icp_results ):
                                          + '{: .8f}) '.format(icp_mse[2]))
 
 
+def sample_cloud (numpy_cloud, sample_factor, deterministic_sampling=False ):
+    '''
+    Samples a cloud by a given factor.
+    '''
+    previous_length = numpy_cloud.shape[0]
+
+    # deterministic sampling
+    if (deterministic_sampling ):
+        numpy_cloud = numpy_cloud[::sample_factor]
+    # random sampling
+    else:
+        indices = random.sample(range(0, numpy_cloud.shape[0] ), int (numpy_cloud.shape[0] / sample_factor ))
+        numpy_cloud = numpy_cloud[indices, :]
+
+    print ("DIM Cloud sampled, factor: "
+           + str(sample_factor )
+           + ". Cloud size / previous cloud size: "
+           + str(numpy_cloud.shape[0] )
+           + "/"
+           + str (previous_length))
+
+    return numpy_cloud
+
+
+def use_icp_on_folder (path_to_folder,
+                       reference_file_tag="reference",
+                       aligned_file_tag=None,
+                       permitted_file_extension=None ):
+
+    # # variables
+    # icp_results = {}        # dictionary to hold icp results
+    #
+    # # crawl path
+    # full_paths = get_all_files_in_subfolders (path_to_folder, permitted_file_extension )
+    # print ("full_paths: " + str (full_paths ))
+    #
+    # # before start, check if files exist
+    # for file_path in full_paths:
+    #     if (input_output.check_for_file (file_path ) is False ):
+    #         print ("File " + file_path + " was not found. Aborting.")
+    #         return False
+    #
+    # # only use files containing string_to_validate
+    # if (reference_file_tag not in reference_file_path):
+    #     continue
+    #
+    # # # treat clouds folder-specific
+    # # find folder name
+    # if (len(complete_file_path.split ('/')) == 1):
+    #     current_folder = ""     # no folder
+    # else:
+    #     current_folder = complete_file_path.split ('/')[-2]
+    #
+    # # check if the folder changed
+    # if (current_folder != previous_folder and reduce_clouds):
+    #
+    #     # all clouds in one folder should get the same trafo
+    #     if ("_reference" in file_path): ?
+    #         # apply icp to all clouds in a folder, use the cloud marked "_reference" as reference
+    #         icp_reference_cloud = numpy_cloud
+    #
+    #     else:
+    #         # the folder is the same -> this is the second file in this folder
+    #
+    #         # sample DIM Clouds
+    #         if ("DSM_Cloud" in file_path):
+    #             aligned_cloud = sample_cloud (aligned_cloud, sample_factor )
+    #
+    #         icp_results.update (do_icp (icp_reference_cloud, numpy_cloud, file_path ))
+    #
+    # numpy_cloud = input_output.load_ascii_file (file_path )
+
+    return 1
+
+
 def get_reduction (numpy_cloud ):
     '''
     Compute the min x and min y coordinate
@@ -164,7 +242,39 @@ def apply_reduction (numpy_cloud, min_x_coordinate, min_y_coordinate ):
     return numpy_cloud
 
 
-def process_clouds (file_extension, reduce_clouds=False, do_normal_calculation=False, apply_icp_algorithm=False):
+def conditionalized_load (file_path ):
+
+    field_labels_list = ['X', 'Y', 'Z']
+    file_name, file_extension = splitext(file_path )
+
+    # # load the file
+    if (file_extension == '.las'):
+        if ("DSM_Cloud" in file_path):
+            # Load DIM cloud
+            numpy_cloud = input_output.load_las_file (file_path, dtype="dim" )
+            field_labels_list.append ('Rf ' 'Gf ' 'Bf ' 'Classification')
+        else:
+            # Load ALS cloud
+            numpy_cloud = input_output.load_las_file (file_path, dtype="als")
+            field_labels_list.append('Intensity '
+                                     'Number_of_Returns '
+                                     'Return_Number '
+                                     'Point_Source_ID '
+                                     'Classification')
+    elif (file_extension == '.asc'):
+        # load ASCII cloud
+        numpy_cloud = input_output.load_ascii_file (file_path )
+        with open(file_path) as f:
+            field_labels_list = f.readline().strip ('//').split ()
+
+    return numpy_cloud, field_labels_list
+
+
+def process_clouds_in_folder (path_to_folder,
+                              permitted_file_extension=None,
+                              string_to_ignore="",
+                              reduce_clouds=False,
+                              do_normal_calculation=False ):
     '''
     Loads all .las files in a given folder. At the users choice, this function also reduces their points so they are
     closer to zero, computes normals for all points and then saves them again with a different name, or applies an icp
@@ -172,18 +282,12 @@ def process_clouds (file_extension, reduce_clouds=False, do_normal_calculation=F
     '''
 
     # crawl path
-    path = "clouds/Regions/"
-    full_paths = get_all_files_in_subfolders (path, file_extension )
-    print ("full_paths: " + str (full_paths[17:] ))
+    full_paths = get_all_files_in_subfolders (path_to_folder, permitted_file_extension )
+    print ("full_paths: " + str (full_paths ))
 
     # # just print paths and quit, if no task was selected
-    # if (not reduce_clouds and not do_normal_calculation and not apply_icp_algorithm ):
+    # if (not reduce_clouds and not do_normal_calculation ):
     #     return True
-
-    # logic check
-    if (apply_icp_algorithm and (reduce_clouds or do_normal_calculation )):
-        print ("Don't reduce clouds or compute their normals while applying icp.\nPlease separate these steps.")
-        return True
 
     # before start, check if files exist
     for file_path in full_paths:
@@ -193,102 +297,59 @@ def process_clouds (file_extension, reduce_clouds=False, do_normal_calculation=F
 
     # set process variables
     previous_folder = ""    # for folder comparison
-    icp_results = {}        # dictionary to hold icp results
-    icp_reference_cloud = None
 
     # process clouds
-    for file_path in full_paths:
+    for complete_file_path in full_paths:
         print ("\n\n-------------------------------------------------------")
 
         # # split path and extension
-        file_name, file_extension = splitext(file_path )
-        # check if it's a .las file, else skip it
-        # if (file_extension != ".las" ):
-        #     continue
+        #file_name, file_extension = splitext(complete_file_path )
 
-        field_labels_list = ['X', 'Y', 'Z']
+        # skip files containing string_to_ignore
+        if (string_to_ignore in complete_file_path):
+            continue
 
-        # if ("ALS" in file_path):
-        #     continue    # normals only on ALS
-
-        # # load the file
-        if (file_extension == '.las'):
-            if ("DSM_Cloud" in file_path):
-                # Load DIM cloud
-                numpy_cloud = input_output.load_las_file (file_path, dtype="dim" )
-                field_labels_list.append ('Rf ' 'Gf ' 'Bf ' 'Classification')
-            else:
-                # Load ALS cloud
-                numpy_cloud = input_output.load_las_file (file_path, dtype="als")
-                field_labels_list.append('Intensity '
-                                         'Number_of_Returns '
-                                         'Return_Number '
-                                         'Point_Source_ID '
-                                         'Classification')
-        elif (file_extension == '.asc'):
-            # load ASCII cloud
-            numpy_cloud = input_output.load_ascii_file (file_path )
-            with open(file_path) as f:
-                field_labels_list = f.readline().strip ('//').split ()
+        # # load
+        numpy_cloud, field_labels_list = conditionalized_load ()
 
         # # treat clouds folder-specific
         # find folder name
-        if (len(file_path.split ('/')) == 1):
-            current_folder = file_path
+        if (len(complete_file_path.split ('/')) == 1):
+            current_folder = ""     # no folder
         else:
-            current_folder = file_path.split ('/')[-2]
+            current_folder = complete_file_path.split ('/')[-2]
 
         # check if the folder changed
-        if (current_folder != previous_folder):
+        if (current_folder != previous_folder and reduce_clouds):
 
-            if ("_reduced" not in file_path and reduce_clouds):
-                # all clouds in one folder should get the same trafo
-                min_x, min_y = get_reduction (numpy_cloud )
-
-            if (apply_icp_algorithm and "_reference" in file_path):
-                # apply icp to all clouds in a folder, use the cloud marked "_reference" as reference
-                icp_reference_cloud = numpy_cloud
-
-        elif (apply_icp_algorithm ):
-            # the folder is the same -> this is the second file in this folder
-
-            # sample DIM Clouds
-            if ("DSM_Cloud" in file_path):
-
-                sample_factor = 6
-
-                # deterministic sampling
-                #numpy_cloud = numpy_cloud[::sample_factor]
-
-                # random sampling
-                indices = random.sample(range(0, numpy_cloud.shape[0] ), int (numpy_cloud.shape[0] / sample_factor ))
-                numpy_cloud = numpy_cloud[indices, :]
-
-                print ("DIM Cloud sampled, factor: " + str(sample_factor ))
-
-            icp_results.update (do_icp (icp_reference_cloud, numpy_cloud, file_path ))
-
-        previous_folder = current_folder
+            # all clouds in one folder should get the same trafo
+            min_x, min_y = get_reduction (numpy_cloud )
 
         # # # alter cloud
         cloud_altered = False
 
         # # reduce cloud
-        # skip files already processed
-        if ("_reduced" not in file_path and reduce_clouds):
+        if (reduce_clouds ):
             numpy_cloud = apply_reduction (numpy_cloud, min_x, min_y )
             cloud_altered = True
 
         # # compute normals on cloud
-        # skip files already processed
-        if ("_normals" in file_path and do_normal_calculation):
-            numpy_cloud, field_labels_list = compute_normals (numpy_cloud, file_path, field_labels_list, 2.5 )
-            cloud_altered = True
+        if (do_normal_calculation ):
+            numpy_cloud, field_labels_list, success = compute_normals (numpy_cloud,
+                                                                       complete_file_path,
+                                                                       field_labels_list,
+                                                                       2.5 )
+
+            # don't change the cloud unless all normals have been computed
+            cloud_altered = success
 
         # save the cloud again
         if (cloud_altered):
-            input_output.save_ascii_file (numpy_cloud, field_labels_list, file_path )
+            input_output.save_ascii_file (numpy_cloud, field_labels_list, complete_file_path )
             #input_output.save_ascii_file (numpy_cloud, field_labels_list, "clouds/tmp/normals2_test.asc" )
+
+        # set current to previous folder for folder-specific computations
+        previous_folder = current_folder
 
     if (apply_icp_algorithm ):
         compare_icp_results (icp_results )
