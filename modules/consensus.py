7f4027676b4df42c8467d27b2f39cdd7196a1a11
luh_busse <jannik.busse@onlinehome.de>
2019-06-11 17:25:47 +0200

    Consensus Cube display changes

--- modules/consensus.py
+++ modules/consensus.py
@@ -24,6 +24,55 @@ def vector_array_distance (xyz_array, compared_xyz_array=None ):
     return output.reshape ((xyz_array.shape[0], 1 ))
 
 
+def display_consensus_cube (consensus_cube, corresponding_cloud_size ):
+
+    # normalize consensus row
+    consensus_cube[:, 3] = consensus_cube[:, 3] / corresponding_cloud_size
+
+    # save the highest and lowest value to display the whole cubus
+    lowest_row = consensus_cube[0, :].reshape (1, 4)
+    highest_row = consensus_cube[-1, :].reshape (1, 4)
+
+    # # thin out the cloud by removing the lowest 60% of results
+    # sort by best consensus and remove the last
+    index = -math.floor (0.1 * consensus_cube.shape[0] )
+
+    consensus_cube.view('i8,i8,i8,i8').sort(order=['f3'], axis=0 )      # .flip (axis=0 )
+    # print ("sorted consensus_cube:\n" + str(consensus_cube) )
+    # print ("consensus_cube.shape[0]: " + str(consensus_cube.shape[0]))
+    # print ("index: " + str(index))
+    consensus_cube = consensus_cube[index:-1, :]
+    # print ("CUT consensus_cube:\n" + str(consensus_cube) )
+    #consensus_cube = consensus_cube[consensus_cube[:, 3] > 0.01]
+
+    # re-append highest and lowest lowest_row
+    consensus_cube = np.concatenate ((lowest_row, consensus_cube, highest_row), axis=0 )
+
+    #print ("normalized_consensus_counts: " + str (normalized_consensus_counts ))
+
+    # # color the plot
+    rgba_colors = np.zeros((consensus_cube[:, 3].shape[0], 4 ))
+
+    #print ("consensus_cube:\n" + str (consensus_cube ))
+
+    # fill the colors
+    rgba_colors[:, 0] = 1 - consensus_cube[:, 3]
+    rgba_colors[:, 1] = consensus_cube[:, 3]
+    rgba_colors[:, 2] = 0.2
+
+    # the fourth column are the alpha values
+    rgba_colors[:, 3] = np.ones ((consensus_cube.shape[0]))     # consensus_cube[:, 3]
+
+    # # plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(consensus_cube[:, 0], consensus_cube[:, 1], consensus_cube[:, 2], c=rgba_colors, marker='o')
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+    plt.show()
+
+
 def cloud_consensus (numpy_cloud, corresponding_cloud, threshold ):
     '''
     Counts points of numpy_cloud that have a neighbor of smaller distance than threshold in the corresponding cloud.
@@ -45,11 +94,11 @@ def cloud_consensus (numpy_cloud, corresponding_cloud, threshold ):
     part_time_1 = time.time () - part_time_1
 
     part_time_2 = time.time ()
-    tree = sklearn.neighbors.kd_tree.KDTree (corresponding_cloud, leaf_size=40, metric='euclidean')
+    tree = sklearn.neighbors.kd_tree.KDTree (numpy_cloud, leaf_size=40, metric='euclidean')
     part_time_2 = time.time () - part_time_2
 
     part_time_3 = time.time ()
-    list_consensus_counts = tree.query_radius (numpy_cloud, threshold, return_distance=False, count_only=True)
+    list_consensus_counts = tree.query_radius (corresponding_cloud, threshold, return_distance=False, count_only=True)
     part_time_3 = time.time () - part_time_3
 
     part_time_4 = time.time ()
@@ -59,7 +108,7 @@ def cloud_consensus (numpy_cloud, corresponding_cloud, threshold ):
     return np.sum(consensus_vector), consensus_vector, (time.time () - start_time, part_time_1, part_time_2, part_time_3, part_time_4)
 
 
-def cubic_cloud_consensus (numpy_cloud, compared_cloud, threshold, cubus_length, step, return_consensus_cloud=False ):
+def cubic_cloud_consensus (numpy_cloud, compared_cloud, threshold, cubus_length, step, show_consensus_cube=True ):
     '''
     Translates compared_cloud in lenghts of step inside a cubus-shaped space and, for every step, checks how many points
     of cloud numpy_cloud have a neighbor within threshold range in compared_cloud.
@@ -126,7 +175,7 @@ def cubic_cloud_consensus (numpy_cloud, compared_cloud, threshold, cubus_length,
                     best_consensus_count = consensus_count
                     best_alignment_consensus_vector = consensus_vector
 
-                if (return_consensus_cloud):
+                if (show_consensus_cube):
                     consensus_cloud[iteration_count, :] = (translation[0],
                                                            translation[1],
                                                            translation[2],
@@ -142,79 +191,38 @@ def cubic_cloud_consensus (numpy_cloud, compared_cloud, threshold, cubus_length,
     print ("Overall Time - Consensus Time: " + str (time.time () - start_time - consensus_round_time))
     print ("Overall Time: " + str (time.time () - start_time ))
 
-    if (return_consensus_cloud ):
-        return best_alignment, best_consensus_count, best_alignment_consensus_vector, consensus_cloud
-    else:
-        return best_alignment, best_consensus_count, best_alignment_consensus_vector
+    print ("best_alignment: " + str(best_alignment ))
+    print ("best_consensus_count: " + str(best_consensus_count ))
 
+    if (show_consensus_cube ):
+        display_consensus_cube (consensus_cloud, corresponding_cloud.shape[0] )
 
-def show_consensus_cube (consensus_cube ):
-
-    # save the highest and lowest value to display the whole cubus
-    lowest_row = consensus_cube[0, :].reshape (1, 4)
-    highest_row = consensus_cube[-1, :].reshape (1, 4)
-
-    # thin out the cloud by removing the
-    consensus_cube = consensus_cube[consensus_cube[:, 3] > 0.01].copy ()
-
-    # re-append highest and lowest lowest_row
-    consensus_cube = np.concatenate ((lowest_row, consensus_cube, highest_row), axis=0 )
-
-    # normalize consensus row
-    normalized_consensus_counts = consensus_cube[:, 3] / np.max(consensus_cube[:, 3])
-
-    #print ("normalized_consensus_counts: " + str (normalized_consensus_counts ))
-
-    # # color the plot
-    rgba_colors = np.zeros((normalized_consensus_counts.size, 4 ))
-
-    #print ("consensus_cube:\n" + str (consensus_cube ))
-
-    # fill the colors
-    rgba_colors[:, 0] = 0.1
-    rgba_colors[:, 1] = normalized_consensus_counts
-    rgba_colors[:, 2] = 0.1
-
-    # the fourth column are the alpha values
-    rgba_colors[:, 3] = normalized_consensus_counts
-
-    # # plot
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(consensus_cube[:, 0], consensus_cube[:, 1], consensus_cube[:, 2], c=rgba_colors, marker='o')
-    ax.set_xlabel('X Label')
-    ax.set_ylabel('Y Label')
-    ax.set_zlabel('Z Label')
-    plt.show()
+    return best_alignment, best_consensus_count, best_alignment_consensus_vector
 
 
 # small cloud
-numpy_cloud = np.random.uniform (-1000, 1000, (100, 3 ))
-
-print ('numpy_cloud.shape: ' + str (numpy_cloud.shape ))
-
-random1 = np.random.uniform ((-1, -1, -1), (1, 1, 1 ))
-print ("random1: " + str(random1 ))
-corresponding_cloud = numpy_cloud + random1
-
-# # big cloud
-# missing_building_als = input_output.load_ascii_file (
-#     'clouds/Regions/Xy Tower/ALS16_Cloud_reduced_normals_cleared.asc' )
+# numpy_cloud = np.random.uniform (-10, 10, (100, 3 ))
 #
-# missing_building_dim = input_output.load_ascii_file (
-#     'clouds/Regions/Xy Tower/DSM_Cloud_reduced_normals.asc' )
+# print ('numpy_cloud.shape: ' + str (numpy_cloud.shape ))
+#
+# random1 = np.random.uniform ((-1, -1, -1), (1, 1, 1 ))
+# corresponding_cloud = numpy_cloud + random1
 
-best_alignment, best_consensus_count, best_alignment_consensus_vector, consensus_cloud = \
+# big cloud
+numpy_cloud = input_output.load_ascii_file (
+    'clouds/Regions/Xy Tower/ALS16_Cloud_reduced_normals_cleared.asc' )
+
+corresponding_cloud = input_output.load_ascii_file (
+    'clouds/Regions/Xy Tower/DSM_Cloud_reduced_normals.asc' )
+
+best_alignment, best_consensus_count, best_alignment_consensus_vector = \
     cubic_cloud_consensus (numpy_cloud,
                            corresponding_cloud,
-                           threshold=.1,
+                           threshold=.11,
                            cubus_length=2,
-                           step=.05,
-                           return_consensus_cloud=True )
-print ("best_alignment: " + str(best_alignment ))
-print ("best_consensus_count: " + str(best_consensus_count ))
+                           step=.2 )
+#print ("random offset: " + str(random1 ))
 #print ("best_alignment_consensus_vector: " + str(best_alignment_consensus_vector ))
-show_consensus_cube (consensus_cloud )
 
 
 # corresponding_cloud = np.array([[1.1, 0, 0],
