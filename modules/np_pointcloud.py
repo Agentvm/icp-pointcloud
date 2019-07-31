4f54bde76cd4a901dc63cc5de7c5b4cd4da44ed0
luh_busse <jannik.busse@onlinehome.de>
2019-07-31 12:48:32 +0200

    Comments and changes in modules/np_pointcloud.py
    Added Module docstring, class docstring and corrcted function 
    docstrings, added comments
    
    Fixed the replace=True option of add_fields(). It now correctly replaces 
    fields that are already contained in the cloud
    
    The input data is now copied in the constructor
    
    The cloud object is now returned in add_fields() and delete_fields()
    
    Printoptions altered slightly

--- modules/np_pointcloud.py
+++ modules/np_pointcloud.py
@@ -1,9 +1,24 @@
+"""
+Module that contains the NumpyPointCloud class, which effectively wraps together a pointcloud and the description of
+it's data fields for precise adressing, addition, replacement and deletion of data rows.
+"""
+
 import numpy as np
 import warnings
 
 
 class NumpyPointCloud (object ):
-    """docstring for NumpyPointCloud ."""
+    """
+    A Class that contains a 2-D numpy.ndarray and field labels describing the content of the arrays rows for easy access
+
+    Functions:
+        get_fields (field_labels_list ):    Extract a copy of one or multiple fields
+        get_xyz_coordinates ():             shorthand for self.get_fields (["X", "Y", "Z"] )
+        get_normals ():                     shorthand for self.get_fields (["Nx", "Ny", "Nz"] )
+        get_normals_and_sigma ():           shorthand for self.get_fields (["Nx", "Ny", "Nz", "Sigma"] )
+        add_fields (field_data, field_labels_list, replace=False ):     Add one or multiple fields
+        delete_fields (field_labels_list, warn=True ):                  Delete one or multiple fields
+    """
 
     def __init__(self, numpy_ndarray=None, field_labels_list=[] ):
         """
@@ -18,31 +33,32 @@ class NumpyPointCloud (object ):
         # # Inheritance
         # super (NumpyPointCloud, self ).__init__ ()
 
-        # Assign labels and data, remove any spaces around the labels
-        #self.field_labels = np.array ([label.strip () for label in field_labels_list] )
-        self.field_labels = [label.strip () for label in field_labels_list]
-        self.points = numpy_ndarray
+        # Assign a copy of labels and data, remove any spaces around the labels
+        self.field_labels = [label.strip () for label in field_labels_list].copy ()
+        self.points = numpy_ndarray.copy ()
 
     def __get_indices (self, field_labels_list ):
-        "Return all indices that correspond to the requested labels"
+        """Returns all indices of fields in this cloud that correspond to the requested labels"""
 
         if (type (field_labels_list) is str):
             field_labels_list = [field_labels_list]
 
         indices = []
         for field in field_labels_list:
-            #indices.append (np.where (field_labels_list == field ))
             indices.append (self.field_labels.index(field ))
 
         return indices
 
     def get_fields (self, field_labels_list ):
-        '''
-        Extract multiple rows of this cloud by name.
+        """
+        Extract one or multiple rows (fields) of this cloud by name. Returns a copy.
 
         Input:
             field_labels_list (list(string)):    The names of the fields to be returned, in a list
-        '''
+
+        Output:
+            data (numpy.ndarray):   The requested row(s). If only one row is fetched, it's shape will be 1 dimensional
+        """
 
         if (type (field_labels_list) is str):
             field_labels_list = [field_labels_list]
@@ -62,27 +78,38 @@ class NumpyPointCloud (object ):
         # return a copy of the requested data
         data = self.points[:, indices].copy ()
 
+        # if only one row is requested, reshape it to 1-D, so boolean comparison doesn't throw an error
         if (data.shape[1] == 1):
             data = data[:, 0]
 
         return data
 
     def get_xyz_coordinates (self ):
+        """Get the (X, Y, Z) coordinates of this cloud (usually the first three fields)"""
         return self.get_fields (["X", "Y", "Z"] )
 
     def get_normals (self ):
+        """Get the three fields containing the normals of this cloud (Nx, Ny, Nz)"""
         return self.get_fields (["Nx", "Ny", "Nz"] )
 
     def get_normals_and_sigma (self ):
+        """Get the three normal vector fields and the Sigma field containing the noise from normal estimation"""
         return self.get_fields (["Nx", "Ny", "Nz", "Sigma"] )
 
     def add_fields (self, field_data, field_labels_list, replace=False ):
-        '''
+        """
+        Add one or more data fields to this cloud, optionally replacing existing fields.
+
         Input:
             field_data (np.ndarray):            Data in the shape of (n, m), where n = number of points in cloud
-            field_labels_list (list(string)):   Labels of field_data, length = m
-        '''
+            field_labels_list (list(string)):   Labels of field_data, length of list = m
+            replace (boolean):                  If True, fields already contained in this cloud are replaced
 
+        Output:
+            self (NumpyPointCloud):             The object of this cloud (optional)
+        """
+
+        # # input checks
         # check type of input
         if (type (field_labels_list ) is str ):
             field_labels_list = [field_labels_list]
@@ -99,36 +126,50 @@ class NumpyPointCloud (object ):
 
         # check number of labels
         if (len (field_labels_list ) != field_data.shape[1] ):
-            raise ValueError ("Supplied data was of shape "
-                              + str (field_data.shape )
-                              + ", but there were "
-                              + str (len (field_labels_list )) + " Labels supplied." )
+            raise ValueError ("Supplied data has " + str (field_data.shape[1] )
+                              + " fields, but there were " + str (len (field_labels_list )) + " Labels supplied." )
 
-        truth_array = [new_field_name in self.field_labels for new_field_name in field_labels_list]
-
+        # # addition
+        # check if any field to be added is already in the cloud
+        # create a truth_array that shows which of the labels in field_labels_list are contained in self.field_labels
+        truth_array = [new_field_name in self.field_labels for new_field_name in field_labels_list]     # is a list
         if (any(truth_array ) and not replace ):
-            raise ValueError ("This Cloud already has fields named \'" + str (np.array(field_labels_list)[truth_array])
-                              + "\'."
-                              + "\nIf you want to replace the data, set replace=True."
+            raise ValueError ("This Cloud already has fields named " + str (np.array(field_labels_list)[truth_array])
+                              + ".\nIf you want to replace the data, set replace=True."
                               + "\nCloud fields are: " + str(self.field_labels ))
+        # replace
+        elif (any(truth_array ) and replace):
+            # replacing the fields already present
+            # prepare field_labels_list for indexing and get the indices of the fields already present in the cloud
+            field_labels_list = np.array (field_labels_list )
+            indices = self.__get_indices (field_labels_list[truth_array] )
 
+            # replace the existing fields with the new data
+            self.points[:, indices] = field_data[:, truth_array]
+
+            # concatenate the rest of the fields (which are not already contained in the cloud)
+            reversed_truth_array = [not boolean for boolean in truth_array]
+            self.points = np.concatenate ((self.points, field_data[:, reversed_truth_array]), axis=1 )
+            self.field_labels += field_labels_list[reversed_truth_array].tolist ()
+
+        # simple add
         else:
-            # delete all fields that need to be overwritten, then append the values to the cloud
-            # refactor: This is time consuming and alters the order of fields
-            self.delete_fields (field_labels_list, warn=False )
-
             self.points = np.concatenate ((self.points, field_data), axis=1 )
             self.field_labels += field_labels_list
 
         return self
 
     def delete_fields (self, field_labels_list, warn=True ):
-        '''
-        Delete multiple rows of this cloud by name.
+        """
+        Delete one or multiple rows of this cloud by name.
 
         Input:
-            requested_fields (list(string)):    The names of the fields to be returned, in a list
-        '''
+            field_labels_list (list(string)):   The names of the fields to be deleted, in a list
+            warn (boolean):                     If True, prints a warning if fields to be deleted could not be found
+
+        Output:
+            self (NumpyPointCloud):             The object of this cloud (optional)
+        """
 
         if (type (field_labels_list) is str):
             field_labels_list = [field_labels_list]
@@ -157,11 +198,17 @@ class NumpyPointCloud (object ):
         for field_to_delete in field_labels_list:
             self.field_labels.remove (field_to_delete )
 
+        return self
+
     def __str__(self ):
-        '''
+        """
         This Method is called when an object of this class is printed.
         It returns a nicely formatted string containing the labels and points.
-        '''
+        """
 
-        return ("\n" + str (self.field_labels )
-                + "\n" + str (self.points ))
+        # change np printoptions to make linebreaks happen later and suppress -3e10 display style
+        np.set_printoptions(precision=6, linewidth=120, suppress=True )
+        string = ("\n" + str (self.field_labels ) + "\n" + str (self.points ))
+        np.set_printoptions ()
+
+        return string
