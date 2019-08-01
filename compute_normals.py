Normal computation radius limitation fixed, speed improvement ~100%
compute_normals.py: Tree is queried in batches

modules/normals.py: Ransac algorithm refactored: Works with arrays now, 
all random planes are computed up front
CodeCleaning still to do

compute_normals.py
@@ -7,6 +7,7 @@ from modules import normals
 import numpy as np
 import random
 import os.path
+import time
 
 # advanced functionality
 import scipy.spatial
@@ -41,6 +42,17 @@ def compute_normals (numpy_pointcloud, file_path, query_radius ):
 
     """
 
+    # outer_iterations = 0
+    # inner_iterations = 0
+    outer_loop_query_time = 0
+    inner_loop_computation_time = 0
+    overall_ransac_time = 0
+    overall_random_plane_time = 0
+    overall_plane_consensus_time = 0
+    overall_pca_time = 0
+    start_time = time.time ()
+    measure = time.time ()
+
     # build a kdtree
     #tree = sklearn.neighbors.kd_tree.KDTree (numpy_cloud[:, 0:3], leaf_size=40, metric='euclidean')
     scipy_kdtree = scipy.spatial.cKDTree (numpy_pointcloud.get_xyz_coordinates () )
@@ -49,56 +61,130 @@ def compute_normals (numpy_pointcloud, file_path, query_radius ):
     additional_values = np.zeros ((numpy_pointcloud.points.shape[0], 4 ))
     success = True
 
+    init_time = time.time () - measure
+    measure = time.time ()
+
     # set radius
-    if ("DSM_Cloud" in file_path):
-        query_radius = 0.8
+    # if ("DSM_Cloud" in file_path):
+    #     query_radius = 0.8
 
     # compute normals for each point
-    for index, point in enumerate (numpy_pointcloud.get_xyz_coordinates () ):
+    #for index, point in enumerate (numpy_pointcloud.get_xyz_coordinates () ):
+    errors = 0
+    step = 10000
+    cloud_points_number = numpy_pointcloud.points.shape[0]
+    for i in range (0, cloud_points_number, step ):
+        interim = time.time ()
+        if (i + step > cloud_points_number):
+            batch_points = numpy_pointcloud.points[i:, 0:3]
+        else:
+            batch_points = numpy_pointcloud.points[i:i+step, 0:3]
 
         # check memory usage
         if (psutil.virtual_memory().percent > 95.0):
             print (print ("!!! Memory Usage too high: "
                           + str(psutil.virtual_memory().percent)
                           + "%. Skipping cloud. There still are "
-                          + str (numpy_pointcloud.points.shape[0] - index)
+                          + str (numpy_pointcloud.points.shape[0] - i)
                           + " normal vectors left to compute. Reduction process might be lost."))
             success = False
             break
 
-        if (numpy_pointcloud.points.shape[0] > 10 and index % int(numpy_pointcloud.points.shape[0] / 10) == 0):
-            print ("Progress: " + "{:.1f}".format ((index / numpy_pointcloud.points.shape[0]) * 100.0 ) + " %" )
+        #if (numpy_pointcloud.points.shape[0] > 10 and i % int(numpy_pointcloud.points.shape[0] / 10) == 0):
+        print ("Progress: " + "{:.1f}".format ((i / numpy_pointcloud.points.shape[0]) * 100.0 ) + " %" )
 
         # kdtree radius search
         #point_neighbor_indices = tree.query_radius(point.reshape (1, -1), r=query_radius )
-        point_neighbor_indices = scipy_kdtree.query_ball_point(point.reshape (1, -1), r=query_radius )
+        batch_point_indices = scipy_kdtree.query_ball_point (np.ascontiguousarray(batch_points ), r=query_radius )
 
-        # just get all indices in the point radius
-        point_neighbor_indices = [nested_value for value in point_neighbor_indices for nested_value in value]
+        # # just get all indices in the point radius
+        # # refactor: This is the problem why batches do not work
 
-        # make kdtree smaller in DSM clouds to avoid too many matches slowing the process down
-        if (len (point_neighbor_indices ) > 500):
-            indices = random.sample(range(0, len (point_neighbor_indices ) ), int (len (point_neighbor_indices ) / 5 ))
-            point_neighbor_indices = [point_neighbor_indices[i] for i in indices]
+        # print ("\n--- --- --- --- --- --step: " + str(batch_point_indices.shape[0] ))
 
-        # you can't estimate a cloud with less than three neighbors
-        if (len (point_neighbor_indices) < 3 ):
-            continue
+        outer_loop_query_time += time.time () - interim
 
-        # do a Principal Component Analysis with the plane points obtained by a RANSAC plane estimation
-        normal_vector, sigma, mass_center = normals.PCA (
-                    normals.ransac_plane_estimation (numpy_pointcloud.points[point_neighbor_indices, :],    # neighbors
-                                                     threshold=0.3,  # max point distance from the plane
-                                                     fixed_point=numpy_pointcloud.points[index, :],
-                                                     w=0.6,         # probability for the point to be an inlier
-                                                     z=0.90)        # desired probability that plane is found
-                                                     [1] )          # only use the second return value, the points
+        for t, point_neighbor_indices in enumerate (batch_point_indices, 0 ):
 
-        # join the normal_vector and sigma value to a 4x1 array and write them to the corresponding position
-        additional_values[index, :] = np.append (normal_vector, sigma )
+            iterator = i + t
+
+            # if (t == 0):
+            #     print ("T stars at 0")
+
+            interim = time.time ()
+
+            # print ("point_indices: " + str(point_neighbor_indices ))
+
+            # diff = numpy_pointcloud.points[point_neighbor_indices, 0:3] - point
+            # print ("max_distance = " + str (np.max (np.sqrt (diff[0]**2 + diff[1]**2 + diff[2]**2 ))))
+
+            # # make kdtree smaller in DSM clouds to avoid too many matches slowing the process down
+            # if (len (point_neighbor_indices ) > 500):
+            #     indices = random.sample(range(0, len (point_neighbor_indices ) ),
+            #                             int (len (point_neighbor_indices ) / 5 ))
+            #     point_neighbor_indices = [point_neighbor_indices[i] for i in indices]
+
+            # you can't estimate a cloud with less than three neighbors
+            if (len (point_neighbor_indices) < 3 ):
+                continue
+
+            # # do a Principal Component Analysis with the plane points obtained by a RANSAC plane estimation
+            # normal_vector, sigma, mass_center, _ = normals.PCA (
+            #             normals.ransac_plane_estimation_new (numpy_pointcloud.points[point_neighbor_indices, :],   # point neighbors
+            #                                              threshold=0.3,  # max point distance from the plane
+            #                                              fixed_point=numpy_pointcloud.points[iterator, :],
+            #                                              w=0.6,         # probability for the point to be an inlier
+            #                                              z=0.90)        # desired probability that plane is found
+            #                                              [1] )          # only use the second return value, the points
+
+            _, points, random_plane_time, plane_consensus_time, ransac_time = normals.ransac_plane_estimation_new (
+                                             numpy_pointcloud.points[point_neighbor_indices, :],
+                                             threshold=0.3,  # max point distance from the plane
+                                             fixed_point=numpy_pointcloud.points[i, :],
+                                             w=0.6,         # probability for the point to be an inlier
+                                             z=0.90)        # desired probability that plane is found
+
+            overall_random_plane_time += random_plane_time
+            overall_plane_consensus_time += plane_consensus_time
+            overall_ransac_time += ransac_time
+
+            # do a Principal Component Analysis with the plane points obtained by a RANSAC plane estimation
+            normal_vector, sigma, mass_center, pca_time = normals.PCA (points )
+            overall_pca_time += pca_time
+
+            if (normal_vector[0] == 0 and normal_vector[1] == 0 and normal_vector[2] ):
+                errors += 1
+
+            # join the normal_vector and sigma value to a 4x1 array and write them to the corresponding position
+            additional_values[iterator, :] = np.append (normal_vector, sigma )
+
+            inner_loop_computation_time += time.time () - interim
+
+        #     inner_iterations += 1
+        #
+        # outer_iterations += 1
+
+    interim = time.time ()
 
     # add the newly computed values to the cloud
     numpy_pointcloud.add_fields (additional_values, ['Nx', 'Ny', 'Nz', 'Sigma'], replace=True )
+
+    outro_time = time.time () - interim
+
+    loop_overall_time = time.time () - measure
+    overall_time = time.time () - start_time
+
+    print ("\nstep:            " + str (step ))
+    print ("Init Time:         " + str (init_time ))
+    print ("Overall Loop Time: " + str (loop_overall_time ))
+    print ("\tOuter Loop Time: " + "{:2f}%".format ((outer_loop_query_time / loop_overall_time ) * 100 ))
+    print ("\tRANSAC all Time:  " + "{:2f}%".format ((overall_ransac_time / loop_overall_time ) * 100 ))
+    print ("\t\tRANSAC rndm Time: " + "{:2f}%".format ((overall_random_plane_time / overall_ransac_time ) * 100 ))
+    print ("\t\tRANSAC cnss Time: " + "{:2f}%".format ((overall_plane_consensus_time / overall_ransac_time ) * 100 ))
+    print ("\tPCA Time:         " + "{:2f}%".format ((overall_pca_time / loop_overall_time ) * 100 ))
+
+    print ("Outro Time:        " + str (outro_time ))
+    print ("Overall Time:      " + str (overall_time ))
 
     return numpy_pointcloud, success
 
@@ -205,7 +291,7 @@ def process_clouds_in_folder (path_to_folder,
         # save the cloud again
         if (cloud_altered):
             alteration_string = "_reduced" if reduce_clouds else ""
-            alteration_string += "_normals" if do_normal_calculation else ""
+            alteration_string += ("_normals_r_" + str (normals_computation_radius )) if do_normal_calculation else ""
             alteration_string += "_cleared" if clear_classes else ""
             filename, file_extension = os.path.splitext(complete_file_path )
             input_output.save_ascii_file (np_pointcloud.points,
@@ -228,11 +314,11 @@ if __name__ == '__main__':
     # # normals / reducing clouds
     if (process_clouds_in_folder ('clouds/tmp/',
                                   permitted_file_extension='.asc',
-                                  string_list_to_ignore=['original_clouds', 'fix', 'fail', '.las'],
+                                  string_list_to_ignore=['original_clouds', '_r_', 'fail', '.las'],
                                   do_normal_calculation=True,
-                                  reduce_clouds=True,
+                                  reduce_clouds=False,
                                   clear_classes=False,
-                                  normals_computation_radius=2.5 )):
+                                  normals_computation_radius=1 )):
         print ("\n\nAll Clouds successfully processed.")
     else:
         print ("Error. Not all clouds could be processed.")

modules/normals.py
@@ -4,7 +4,7 @@ Offers Principal Component Analysis and RANSAC algorithms for normal vector comp
 
 
 # basic imports
-#import time
+import time
 import numpy as np
 import random
 from math import ceil, sqrt
@@ -100,12 +100,12 @@ def PCA (numpy_cloud ):
         mass_center ([1, 3] np.array):   Centre of mass
     """
 
-    #start_time = time.time()
+    start_time = time.time()
 
     # abort, if there are no points
     if (numpy_cloud.shape[0] == 0):
         #print ("In normals.py, in PCA: The input array is empty. Returning a null vector and high sigma")
-        return np.array ((0, 0, 0)), 100.0, np.array ((0, 0, 0))
+        return np.array ((0, 0, 0)), 100.0, np.array ((0, 0, 0)), time.time() - start_time
 
     # we only need three colums [X, Y, Z, I] -> [X, Y, Z]
     numpy_cloud = numpy_cloud[:, :3].copy ()     # copying takes roughly 0.000558 seconds per 1000 points
@@ -126,7 +126,7 @@ def PCA (numpy_cloud ):
 
     #print ('PCA completed in ' + str(time.time() - start_time) + ' seconds.\n' )
 
-    return normal_vector, sigma, mass_center
+    return normal_vector, sigma, mass_center, time.time() - start_time
 
 
 def random_plane_estimation (numpy_cloud, fixed_point=None ):
@@ -140,9 +140,15 @@ def random_plane_estimation (numpy_cloud, fixed_point=None ):
         normal_vector ([1, 3] np.array):
         plane_parameter_d (float):
     '''
+    start_time = time.time()
+
+    measure = time.time ()
 
     # get 3 random indices
     idx_1, idx_2, idx_3 = random.sample(range(0, numpy_cloud.shape[0] ), 3 )
+
+    part_1_time = time.time () - measure
+    measure = time.time ()
 
     if (fixed_point is None):
         point_1 = numpy_cloud [idx_1, :].copy ()
@@ -151,15 +157,62 @@ def random_plane_estimation (numpy_cloud, fixed_point=None ):
     point_2 = numpy_cloud [idx_2, :].copy ()
     point_3 = numpy_cloud [idx_3, :].copy ()
 
+    part_2_time = time.time () - measure
+    measure = time.time ()
+
     # get the normal vector, normalize it and if it's turned to the ground, turn it around
     normal_vector = normalize_vector (np.cross((point_2 - point_1), (point_3 - point_1 )))
+
+    part_3_time = time.time () - measure
+    measure = time.time ()
+
     plane_parameter_d = -(normal_vector[0] * point_1[0]
                           + normal_vector[1] * point_1[1]
                           + normal_vector[2] * point_1[2] )
     if (normal_vector[2] < 0):      # z component
         normal_vector = normal_vector * -1
 
-    return normal_vector, plane_parameter_d
+    part_4_time = time.time () - measure
+    measure = time.time ()
+
+    all = part_1_time + part_2_time + part_3_time + part_4_time
+
+    # print ("\npart 1 : " + "{:2f}%".format ((part_1_time / all ) * 100 ))
+    # print ("part 2 : " + "{:2f}%".format ((part_2_time / all ) * 100 ))
+    # print ("part 3 : " + "{:2f}%".format ((part_3_time / all ) * 100 ))
+    # print ("part 4 : " + "{:2f}%".format ((part_4_time / all ) * 100 ))
+
+    return normal_vector, plane_parameter_d, time.time() - start_time
+
+
+def plane_consensus_II (points, normal_vector, d, threshold ):
+    '''
+    Counts points that have a smaller distance than threshold from a given plane
+
+    Input:
+        points ([n, 3] np.ndarray):
+        normal_vector ([1, 3] np.ndarray):
+        d (float):                          Plane parameter d
+        threshold (float):
+
+    Output:
+        consensus_count (int):
+        consensus_points ([[x,y,z], ...] list)
+    '''
+    start_time = time.time()
+
+    # create array for fast computation
+    # normal_d = np.append (normal_vector, d)
+    # array = np.concatenate ((np.tile (normal_d, (points.shape[0], 1) ), points ), axis=1 )
+
+    #dists = np.absolute (np.apply_along_axis (distance_from_plane, 1, array ))
+    dists = (normal_vector[0] * points[:, 0]
+            + normal_vector[1] * points[:, 1]
+            + normal_vector[2] * points[:, 2]
+            + d )
+    consensus_vector = np.where (dists < threshold, True, False )
+
+    return np.sum (consensus_vector), points[consensus_vector, :], time.time() - start_time
 
 
 def plane_consensus (numpy_cloud, normal_vector, d, threshold ):
@@ -176,6 +229,7 @@ def plane_consensus (numpy_cloud, normal_vector, d, threshold ):
         consensus_count (int):
         consensus_points ([[x,y,z], ...] list)
     '''
+    start_time = time.time()
 
     # plane paramters are elements of the normal vector
     a = normal_vector[0]
@@ -198,10 +252,68 @@ def plane_consensus (numpy_cloud, normal_vector, d, threshold ):
             consensus_count = consensus_count + 1     # counting consensus
             consensus_points.append (point.tolist ())     # this might be slowing the code down
 
-    return consensus_count, consensus_points
+    return consensus_count, consensus_points, time.time() - start_time
 
 
-def ransac_plane_estimation (numpy_cloud, threshold, fixed_point=None, w = .9, z = 0.95 ):
+def ransac_plane_estimation (input_numpy_cloud, threshold, fixed_point=None, w = .9, z = 0.95 ):
+    """
+    Uses Ransac with the probability parameters w and z to estimate a valid plane in given cloud.
+    Uses distance from plane compared to given threshold to determine the consensus set.
+    Returns points and point indices of the detected plane.
+    Input:
+        input_numpy_cloud (np.array):   Input cloud
+        threshold (float, in m):        Points closer to the plane than this value are counted as inliers
+        fixed_point (int):              This point will be used as one of three points for every plane estimation
+        w (float between 0 and 1):      probability that any observation belongs to the model
+        z (float between 0 and 1):      desired probability that the model is found
+    Output:
+        consensus_normal_vector ([1, 3] np.array):  The normal_vector computed
+        consensus_points (np.array):                All points used for plane estimation
+    """
+
+    # measure time
+    start_time = time.time ()
+
+    # variables
+    current_consensus = 0
+    best_consensus = 0
+    consensus_points = []  # points matching the cloud
+    consensus_normal_vector = []
+
+    # determine probabilities and number of draws
+    b = np.float_power(w, 3 )   # probability that all three observations belong to the model
+    k = ceil(np.log(1-z ) / np.log(1-b ))   # number of draws
+
+    # copy cloud
+    numpy_cloud = input_numpy_cloud[:, 0:3].copy ()
+
+    # iterate: draw 3 points k times
+    for i in range (1, k):
+
+        # estimate a plane with 3 random points
+        normal_vector, d, _ = random_plane_estimation (numpy_cloud, fixed_point )
+
+        # this happens if three points are the same or on a line
+        if (np.sum (normal_vector ) == 0 ):
+            #print ("this happens if three points are the same or on a line")
+            continue
+
+        # count all points that consent with the plane
+        current_consensus, current_consensus_points, _ = plane_consensus (numpy_cloud, normal_vector, d, threshold )
+
+        # is the current consensus match higher than the previous ones?
+        if (current_consensus > best_consensus ):
+            consensus_points = current_consensus_points
+            best_consensus = current_consensus    # keep best consensus set
+            consensus_normal_vector = normal_vector
+
+    # print time
+    #print('RANSAC completed in ' + str(time.time() - start_time) + ' seconds.\n' )
+
+    return np.array (consensus_normal_vector), np.array (consensus_points).copy (), 1, 1, 1
+
+
+def ransac_plane_estimation_new (numpy_cloud, threshold, fixed_point=None, w = .9, z = 0.95 ):
     """
     Uses Ransac with the probability parameters w and z to estimate a valid plane in given cloud.
     Uses distance from plane compared to given threshold to determine the consensus set.
@@ -219,33 +331,80 @@ def ransac_plane_estimation (numpy_cloud, threshold, fixed_point=None, w = .9, z
     """
 
     # measure time
-    #start_time = time.time ()
+    overall_random_plane_time = 0
+    overall_plane_consensus_time = 0
+    start_time = time.time ()
 
     # variables
     current_consensus = 0
     best_consensus = 0
-    consensus_points = []  # points matching the cloud
+    consensus_points = np.array([])  # points matching the cloud
     consensus_normal_vector = []
 
     # determine probabilities and number of draws
     b = np.float_power(w, 3 )   # probability that all three observations belong to the model
     k = ceil(np.log(1-z ) / np.log(1-b ))   # number of draws
 
-    # # copy cloud
-    numpy_cloud = numpy_cloud[:, 0:3]
+    # # # copy cloud
+    numpy_cloud = numpy_cloud[:, 0:3].copy ()
 
-    # iterate: draw 3 points k times
-    for i in range (1, k):
+    # # iterate: draw 3 points k times
+    # for i in range (1, k):
+    #
+    #     # estimate a plane with 3 random points
+    #     normal_vector, d, random_plane_time = random_plane_estimation (numpy_cloud, fixed_point )
+    #     overall_random_plane_time += random_plane_time
 
-        # estimate a plane with 3 random points
-        [normal_vector, d] = random_plane_estimation (numpy_cloud, fixed_point )
+    measure = time.time ()
 
-        # this happens if three points are the same or on a line
-        if (np.sum (normal_vector ) == 0 ):
-            continue
+    # get 3 * k random indices
+    indices = np.random.random_integers (0, numpy_cloud.shape[0] - 1, 3 * k )    # .reshape (-1, 3)
+
+    # print ("\nindices, cloud")
+    # print (indices.shape)
+    # print (numpy_cloud.shape)
+
+    points = numpy_cloud[indices, 0:3].copy ()
+
+    points = points.reshape (-1, 9)
+    points_1 = points[:, 0:3]
+    points_2 = points[:, 3:6]
+    points_3 = points[:, 6:9]
+
+    # fixed point
+    if (fixed_point is not None ):
+        points_1 = fixed_point[0:3].reshape (-1, 3)  # np.tile (fixed_point, (points_2.shape[0], 1 ))
+
+    # print ("Poins")
+    # print (points.shape)
+    # print (points_1.shape)
+    # print (points_2.shape)
+    # print (points_3.shape)
+
+    # get the normal vector, normalize it
+    normal_vectors = normalize_vector_array (np.cross((points_2 - points_1), (points_3 - points_1 )))
+
+    # get plane parameter d, distance from origin
+    plane_parameters_d = -(normal_vectors[:, 0] * points_1[:, 0]
+                          + normal_vectors[:, 1] * points_1[:, 1]
+                          + normal_vectors[:, 2] * points_1[:, 2] )
+
+    # if it's turned to the ground, turn it around
+    normal_vectors = np.where (normal_vectors[2] < 0, normal_vectors * -1, normal_vectors )
+
+    overall_random_plane_time = time.time () - measure
+
+    for (normal_vector, d) in zip (normal_vectors, plane_parameters_d ):
+
+        # print ("\nnormal_vector array: " + str (normal_vector ))
+        # print ("normal_vector for:   " + str (n_2 ))
+        # print ("d array: " + str (d ))
+        # print ("d for:   " + str (d_2 ))
 
         # count all points that consent with the plane
-        current_consensus, current_consensus_points = plane_consensus (numpy_cloud, normal_vector, d, threshold )
+        current_consensus, current_consensus_points, plane_consensus_time = \
+            plane_consensus_II (numpy_cloud, normal_vector, d, threshold )
+        overall_plane_consensus_time += plane_consensus_time
 
         # is the current consensus match higher than the previous ones?
         if (current_consensus > best_consensus ):
@@ -256,7 +415,7 @@ def ransac_plane_estimation (numpy_cloud, threshold, fixed_point=None, w = .9, z
     # print time
     #print('RANSAC completed in ' + str(time.time() - start_time) + ' seconds.\n' )
 
-    return np.array (consensus_normal_vector), np.array (consensus_points)
+    return np.array (consensus_normal_vector), consensus_points, overall_random_plane_time, overall_plane_consensus_time, time.time() - start_time
 
 
 # set the random seed for both the numpy and random module, if it is not already set.

test.py
@@ -13,7 +13,55 @@ numpy_cloud = np.array([[1.1, 2.1, 3.1],
                         [1.4, 2.4, 3.4],
                         [1.5, 2.5, 3.5],
                         [1.6, 2.6, 3.6]] )
+
 #
+
+# # speed test accumulator init
+# import math
+# import time
+#
+#
+# def create_closed_grid (grid_length, step ):
+#
+#     # grid variables
+#     steps_number = math.ceil (grid_length / step + 1 )
+#     grid_points_number = steps_number**3
+#
+#     # make a grid in the style of a pointcloud
+#     grid = np.zeros ((grid_points_number, 4 ))
+#
+#     # in intervals of step, create grid nodes
+#     general_iterator = 0
+#     minimum = -math.floor (steps_number / 2)
+#     maximum = math.ceil (steps_number / 2 )
+#     for x_iterator in range (minimum, maximum ):
+#         for y_iterator in range (minimum, maximum ):
+#             for z_iterator in range (minimum, maximum ):
+#
+#                 grid[general_iterator, 0:3] = [x_iterator * step,
+#                                                y_iterator * step,
+#                                                z_iterator * step]
+#
+#                 general_iterator += 1
+#
+#     return grid
+#
+#
+# measure = time.time ()
+# grid = create_closed_grid (2, 0.1 )
+# grid_time = time.time - measure
+#
+# print ("grid.shape: " + str (grid.shape ))
+# # print ("grid:\n" + str (grid ))
+#
+# measure = time.time ()
+# lmax = 1
+# lmin = -lmax
+# step = 0.1
+# xyz = np.transpose(np.reshape(np.mgrid[lmin:lmax+step:step, lmin:lmax+step:step, lmin:lmax+step:step], (-1, 4)))
+# print ("\nxyz.shape: " + str (grid.shape ))
+# # print ("xyz:\n" + str (grid ))
+# xyz = time.time - measure
 
 
 # # Test np_pointcloud class NumpyPointCloud - This will throw warnings (and expected errors, if # are removed )
@@ -21,7 +69,7 @@ numpy_cloud = np.array([[1.1, 2.1, 3.1],
 #
 # numpy_cloud = np.concatenate ((numpy_cloud, numpy_cloud), axis=1 )
 # my_cloud = NumpyPointCloud (numpy_cloud, ["X", "Y", "Z", "I", "Don't", "Know"] )
-#
+
 # #
 # # print ("Test: Wrong Get." + str (my_cloud.get_fields (["X", "Y", "Z", "I", "Dont", "Know"] )))
 # # print ("Test: Wrong Get." + str (my_cloud.get_fields (["Dont"] )))
@@ -32,10 +80,13 @@ numpy_cloud = np.array([[1.1, 2.1, 3.1],
 # # my_cloud.add_fields (numpy_cloud[:, 2], "Know" )
 # print ("\nTest: Wrong Replace." + str (my_cloud ))
 #
+# print (my_cloud.shape)
 # # replace
-# my_cloud.add_fields (np.array ([1.1, 2.1, 1337, 1.1, 2.1, 3.1]).reshape(-1, 1), "Know", replace=True )
+# my_cloud.add_fields (np.array ([1.1, 2.1, 1337, 1.1, 2.1, 3.1]).reshape(-1, 1), "aaa", replace=True )
 # my_cloud.add_fields (numpy_cloud[:, 1], "I", replace=True )
 # print ("\nTest: Replace." + str (my_cloud ))
+#
+# print (my_cloud.shape)
 #
 # # add
 # my_cloud.add_fields ([1.1, 2.1, 3.1, 1337, 2.1, 3.1], "Test1" )
@@ -139,7 +190,6 @@ numpy_cloud = np.array([[1.1, 2.1, 3.1],
 
 # # Plot an angle histogram of the differences of normal vectors
 # from modules import input_output
-# from modules.conversions import get_fields
 # from modules.normals import normalize_vector_array, normalize_vector
 # import matplotlib.pyplot as plt
 # import scipy.spatial
@@ -148,13 +198,13 @@ numpy_cloud = np.array([[1.1, 2.1, 3.1],
 # def load_example_cloud (folder ):
 #
 #     # # big cloud
-#     numpy_cloud, numpy_cloud_field_labels = input_output.conditionalized_load(
+#     numpy_pointcloud = input_output.conditionalized_load(
 #         'clouds/Regions/' + folder + '/ALS16_Cloud_reduced_normals_cleared.asc' )
 #
-#     corresponding_cloud, corresponding_cloud_field_labels = input_output.conditionalized_load (
+#     corresponding_pointcloud = input_output.conditionalized_load (
 #         'clouds/Regions/' + folder + '/DSM_Cloud_reduced_normals.asc' )
 #
-#     return numpy_cloud, numpy_cloud_field_labels, corresponding_cloud, corresponding_cloud_field_labels
+#     return numpy_pointcloud, corresponding_pointcloud
 #
 #
 # def einsum_angle_between (vector_array_1, vector_array_2 ):
@@ -165,60 +215,61 @@ numpy_cloud = np.array([[1.1, 2.1, 3.1],
 #     return np.arccos (diag )
 #
 #
-# def plot_histogram (data, bins ):
+# def plot_histogram (data, numer_of_bins, maximum ):
 #     # the histogram of the data
-#     n, bins, patches = plt.hist(data, bins, density=False, range=(0, 180), facecolor='g', alpha=0.75 )
+#     n, bins, patches = plt.hist(data, numer_of_bins, density=False, range=(0, 180), facecolor='g', alpha=0.75 )
 #
 #     plt.xlabel('angle' )
 #     plt.ylabel('count' )
 #     plt.title('Histogram of Angle Differences Yz Houses translated' )
 #     #plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
-#     plt.axis([0, 180, 0, 200000] )
+#     plt.axis([0, numer_of_bins, 0, maximum] )
 #     plt.grid(True )
 #     plt.show()
 #
 #
-# def get_points_normals_zero (numpy_cloud, field_labels_list):
-#     normals = get_fields (numpy_cloud, field_labels_list, ["Nx", "Ny", "Nz"] )
+# def get_points_normals_zero (numpy_pointcloud, field_labels_list):
+#     normals = numpy_pointcloud.get_normals ()
 #     normals = np.absolute (normals )
 #
 #     sqrt = np.sqrt (normals[:, 0]**2 + normals[:, 1]**2 + normals[:, 2]**2 )
 #
 #     a = np.where (sqrt > 0.5, True, False )
 #
-#     return numpy_cloud[a, :]
-#
-#     #return points
+#     return numpy_pointcloud.points[a, :]
 #
 #
 # # load clouds
-# numpy_cloud, numpy_cloud_field_labels, corresponding_cloud, corresponding_cloud_field_labels \
-#      = load_example_cloud ("Yz Houses" )
+# # numpy_pointcloud, corresponding_pointcloud = load_example_cloud ("Yz Houses" )
 #
-# # numpy_cloud = get_points_normals_zero (numpy_cloud, numpy_cloud_field_labels )
-# # corresponding_cloud = get_points_normals_zero (corresponding_cloud, corresponding_cloud_field_labels )
+# numpy_pointcloud = input_output.load_ascii_file ("clouds/tmp/fail/normals_fixpoint_2.asc" )
+# corresponding_pointcloud = input_output.load_ascii_file ("clouds/tmp/fail/normals_fixpoint_3.asc" )
+#
+# # numpy_pointcloud.points = get_points_normals_zero (numpy_pointcloud.points, numpy_cloud_field_labels )
+# # corresponding_pointcloud.points = get_points_normals_zero (
+# #                                       corresponding_pointcloud.points, corresponding_cloud_field_labels )
 #
 # # translate
-# corresponding_cloud += (0.314620971680, -0.019294738770, -0.035737037659, 0, 0, 0, 0, 0, 0, 0, 0 )
+# corresponding_pointcloud.points[:, 0:3] += (0.314620971680, -0.019294738770, -0.035737037659 )
 #
 # # extract normals
-# normals_numpy_cloud = get_fields (numpy_cloud, numpy_cloud_field_labels, ["Nx", "Ny", "Nz"] )
-# normals_corresponding_cloud = get_fields (corresponding_cloud, corresponding_cloud_field_labels, ["Nx", "Ny", "Nz"] )
+# normals_numpy_cloud = numpy_pointcloud.get_normals ()
+# normals_corresponding_cloud = corresponding_pointcloud.get_normals ()
 #
 # # normalize
 # normals_numpy_cloud = normalize_vector_array (normals_numpy_cloud )
 # normals_corresponding_cloud = normalize_vector_array (normals_corresponding_cloud )
 #
 # # build a kdtree and query it
-# kdtree = scipy.spatial.cKDTree (numpy_cloud[:, 0:3] )
-# distances, correspondences = kdtree.query (corresponding_cloud[:, 0:3], k=1 )
+# kdtree = scipy.spatial.cKDTree (numpy_pointcloud.points[:, 0:3] )
+# distances, correspondences = kdtree.query (corresponding_pointcloud.points[:, 0:3], k=1 )
 #
 # # get the angle differences between the normal vectors
 # angle_differences = einsum_angle_between (normals_numpy_cloud[correspondences, :],
 #                                           normals_corresponding_cloud ) * (180/np.pi)
 #
 # # plot
-# plot_histogram (angle_differences, 180 )
+# plot_histogram (angle_differences, 180, 12000 )
 #
 # # corresponding_cloud = np.concatenate (
 # #     (corresponding_cloud, angle_differences.reshape (-1, 1 )), axis=1 )
@@ -833,63 +884,46 @@ numpy_cloud = np.array([[1.1, 2.1, 3.1],
 # from modules import normals
 # from modules import input_output
 # import sys
-#
-# # ply files
-# numpy_cloud_1 = input_output.load_ply_file ('clouds/laserscanning/', 'plane1.ply')    # 3806 points
-# #numpy_cloud_2 = input_output.load_ply_file ('clouds/laserscanning/', 'plane2.ply')    # 3806 points
-#
-# # las files
-# #numpy_cloud_1 = input_output.load_las_file ('clouds/laserscanning/plane1.las')    # 3806 points
-# #numpy_cloud_2 = input_output.load_las_file ('clouds/laserscanning/plane2.las')    # 3806 points
-#
-# # # simple plane
-# # numpy_cloud_1 = np.array ([[1, 0, 0],   # +x
-# #                           [2, 1, 0],  # -x
-# #                           [0, 1, 0],
-# #                           [3, 1.5, 0],
-# #                           [-2, 1.5, 0],
-# #                           [3, 1, 0.51]])  # +y
-#
-# # Simple plane OUTPUT
-# # executed with python version 3.5
-# # PCA completed in 0.00033354759216308594 seconds.
 # #
-# # PCA, Cloud 1:
-# # normal_vector: [-0.0400096  -0.10263703  0.99391392]
-# # sigma: 0.17782724826887134
-# # mass_center: [ 1.5   0.5  -0.05]
+# # # ply files
+# # # numpy_cloud_1 = input_output.load_ply_file ('clouds/laserscanning/', 'plane1.ply')    # 3806 points
+# # #numpy_cloud_2 = input_output.load_ply_file ('clouds/laserscanning/', 'plane2.ply')    # 3806 points
 # #
-# # index: [2, 3, 1]
-# # index: [0, 2, 1]
-# # RANSAC completed in 0.0004112720489501953 seconds.
+# # # las files
+# # #numpy_cloud_1 = input_output.load_las_file ('clouds/laserscanning/plane1.las')    # 3806 points
+# # #numpy_cloud_2 = input_output.load_las_file ('clouds/laserscanning/plane2.las')    # 3806 points
 # #
-# # RANSAC, Cloud 1:
-# # normal_vector: [0.18814417 0.28221626 0.94072087]
-# # consensus_points:
-# # [[1.0, 0.0, 0.0], [2.0, 0.0, -0.2], [0.0, 1.0, -0.1], [3.0, 1.0, 0.1]]
-# # OUTPUT END
+# # simple plane
+# # numpy_cloud_1 = np.array ([[-1, 0, 0],   # +x
+# #                           [2, 0, 0],  # -x
+# #                           [0, 2, 0]
+# #                           [2, 0, 200]])  # +y
 #
-# #                                                                 matlab: 7926 points
+# numpy_cloud_1 = np.array ([[-1, 0, 0],   # +x
+#                           [2, 0, 0],  # -x
+#                           [0, 2, 0],
+#                           [0, 3, 0],
+#                           [0, 4, 0],
+#                           [0, 5, 0],
+#                           [-1, 0, 200],   # +x
+#                           [2, 0, 200],  # -x
+#                           [0, 2, 200],
+#                           [0, 3, 200],
+#                           [0, 4, 200],
+#                           [0, 5, 200]])  # +y
 #
+# # numpy_cloud_1 = np.random.uniform (-10, 10, (300, 3))
+# #
 # # 1st cloud
-# normal_vector, sigma, mass_center = normals.PCA (numpy_cloud_1 )
-# print ('PCA, Cloud 1:\nnormal_vector: ' + str(normal_vector ))
+# normal_vector, consensus_points, _, _, _ = \
+#     normals.ransac_plane_estimation (numpy_cloud_1, 0.1, fixed_point=numpy_cloud_1[0, :], w=0.8 )
+# print ('\nRANSAC, Cloud 1:\nnormal_vector: ' + str(normal_vector ))
+# print ('consensus_points:\n' + str(consensus_points ) + '\n')
+# #
+# normal_vector, sigma, mass_center, _ = normals.PCA (consensus_points )
+# print ('\nPCA, Cloud 1:\nnormal_vector: ' + str(normal_vector ))
 # print ('sigma: ' + str(sigma ))
 # print ('mass_center: ' + str(mass_center ) + '\n')
-#
-# normal_vector, consensus_points = normals.ransac_plane_estimation (numpy_cloud_1, 0.5 )
-# print ('RANSAC, Cloud 1:\nnormal_vector: ' + str(normal_vector ))
-# print ('consensus_points:\n' + str(consensus_points ) + '\n')
-#
-# # # 2nd cloud
-# # normal_vector, sigma, mass_center = normals.PCA (numpy_cloud_2 )
-# # print ('PCA, Cloud 2:\nnormal_vector: ' + str(normal_vector ))
-# # print ('sigma: ' + str(sigma ))
-# # print ('mass_center: ' + str(mass_center ))
-# #
-# # normal_vector, consensus_points = normals.ransac_plane_estimation (numpy_cloud_2, 0.5 )
-# # print ('RANSAC, Cloud 2:\nnormal_vector: ' + str(normal_vector ))
-# # print ('consensus_points:\n' + str(consensus_points ) + '\n')
 
 
 # corresponding_cloud = np.array([[1.1, 0, 0],